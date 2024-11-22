dd9ed45ef6e5ac70a387b8ecb43eb89c999231bd
John Aderiel Magsino <21-30008@g.batstate-u.edu.ph>
2024-11-22 18:18:17 +0800

    Update installed.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'aa6a705f32bcb739546b3fb9a9b9626fc294d076',
+        'reference' => 'f479b9085d16ea17673b21cb941deedd3b49affe',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'aa6a705f32bcb739546b3fb9a9b9626fc294d076',
+            'reference' => 'f479b9085d16ea17673b21cb941deedd3b49affe',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
