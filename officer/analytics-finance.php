601309fc97dbf9c52b2cdacb29b4aeafd2b489b0
John Aderiel Magsino <21-30008@g.batstate-u.edu.ph>
2024-11-23 00:32:34 +0800

    Update analytics-finance.php

--- officer/analytics-finance.php
+++ officer/analytics-finance.php
@@ -204,7 +204,7 @@ include '../includes/db_connection.php';
                     });
 
                     function fetchRevenueForecast() {
-                        fetch('https://epm-analytics-13715bf8762f.herokuapp.com/predict_finance', { // Updated URL
+                        fetch('http://192.168.1.166:5000/predict_finance', {
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/json'
