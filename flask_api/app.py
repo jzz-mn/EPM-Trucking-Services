2656c9b5648d214ba90c7a431ef0f169705e9aff
John Aderiel Magsino <21-30008@g.batstate-u.edu.ph>
2024-11-22 20:45:55 +0800

    ANALYTICS

--- flask_api/app.py
+++ flask_api/app.py
@@ -32,7 +32,7 @@ except Exception as e:
     maintenance_model = None
 
 # Database setup
-DATABASE_URI = 'mysql://p7apqmgbef3tu2d6:qi0il5aezrqpsji6@ixnzh1cxch6rtdrx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/lnm4m0erp17734x3'
+DATABASE_URI = 'mysql+pymysql://root:@localhost/epm_database'
 engine = create_engine(DATABASE_URI)
 
 
--- officer/analytics-finance.php
+++ officer/analytics-finance.php
@@ -204,7 +204,7 @@ include '../includes/db_connection.php';
                     });
 
                     function fetchRevenueForecast() {
-                        fetch('http://127.0.0.1:5000/predict_finance', {
+                        fetch('https://epm-analytics.herokuapp.com/predict_finance', { // Updated URL
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/json'
