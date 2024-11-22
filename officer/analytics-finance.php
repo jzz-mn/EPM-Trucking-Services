93219f83da270aa3fe58b7f1a7cd3abd6e185ed7
John Aderiel Magsino <21-30008@g.batstate-u.edu.ph>
2024-11-23 00:33:55 +0800

    Update analytics-finance.php

--- officer/analytics-finance.php
+++ officer/analytics-finance.php
@@ -204,7 +204,7 @@ include '../includes/db_connection.php';
                     });
 
                     function fetchRevenueForecast() {
-                        fetch('http://192.168.1.166:5000/predict_finance', {
+                        fetch('http://127.0.0.1:5000/predict_finance', {
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/json'
