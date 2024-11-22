e015b8755a3cb760096abad3c376388b49527d41
John Aderiel Magsino <21-30008@g.batstate-u.edu.ph>
2024-11-22 19:52:34 +0800

    Revert "ANALYTICS DEPLOYMENT"
    This reverts commit d5e784f4175253ebcc848e11c6fc10b53348ad91.

--- officer/analytics-finance.php
+++ officer/analytics-finance.php
@@ -204,7 +204,7 @@ include '../includes/db_connection.php';
                     });
 
                     function fetchRevenueForecast() {
-                        fetch('https://epm-analytics-13715bf8762f.herokuapp.com/', {
+                        fetch('http://127.0.0.1:5000/predict_finance', {
                             method: 'POST',
                             headers: {
                                 'Content-Type': 'application/json'
