eec4246e883f1f147eb4952e9bcbed2790fbc326
John Aderiel Magsino <21-30008@g.batstate-u.edu.ph>
2024-11-23 18:00:56 +0800

    Update db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,30 +1,15 @@
 <?php
-/*
- // Database configuration
- $host = "localhost";  // or "127.0.0.1"
- $usernameDB = "root";   // your MySQL username
- $password = "";       // your MySQL password (leave empty if not set)
- $database = "epm_database";
+// Retrieve connection details from environment variables
+$host = getenv('DB_HOST');
+$usernameDB = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
 
- // Create connection
- $conn = new mysqli($host, $usernameDB, $password, $database);
-
- // Check connection
- if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
- }
-
-*/
-$host = "DB_HOST";  // JawsDB host
-$usernameDB = "DB_USERNAME";  // JawsDB username
-$password = "DB_PASSWORD";    // JawsDB password
-$database = "DB_DATABASE";    // JawsDB database name
-
+// Create connection
 $conn = new mysqli($host, $usernameDB, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
