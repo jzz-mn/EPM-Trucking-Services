b9aa9bf37767106964e6da1fbbd80b61739c4d46
John Aderiel Magsino <21-30008@g.batstate-u.edu.ph>
2024-11-26 05:21:48 +0800

    Update db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,10 +1,9 @@
 <?php
 // Database configuration
-$host = "localhost";  // or "127.0.0.1"
-$usernameDB = "root";   // your MySQL username
-$password = "";       // your MySQL password (leave empty if not set)
-
-$database = "epm_backup";
+$host = getenv('DB_HOST');
+$usernameDB = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
 
 // Create connection
 $conn = new mysqli($host, $usernameDB, $password, $database);
